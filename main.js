42b2b43b24711b70f044c0b88a9559431e86a5a8
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-06-09 13:56:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 addEventListener("load", (event) => {
-    console.log('loaded');
     let bloomSections = document.querySelectorAll('.pre-bloom');
     bloomSections.forEach(el => {
         window.addEventListener("scroll", () => {
                 bloom(el);
             });
     })
-
+    let slideDownSections = document.querySelectorAll('.pre-slide-down');
+    slideDownSections.forEach(el => {
+        window.addEventListener("scroll", () => {
+            slideDown(el);
+        })
+    })
+    let slideLeftItems = document.querySelectorAll('.pre-slide-left');
+    slideLeftItems.forEach(el => {
+        window.addEventListener("scroll", () => {
+            slideLeft(el);
+        })
+    })
 })
 const elementInView = (el, dividend = 1.75) => {
     const elementTop = el.getBoundingClientRect().top;
@@ -18,5 +28,19 @@ function bloom(el){
     if (elementInView(el)){
         el.classList.add('bloom');
         el.classList.remove('pre-bloom');
+    }
+};
+function slideDown(el){
+    if (elementInView(el)){
+        el.classList.add('slide-down');
+        el.classList.remove('pre-slide-down');
+    }
+};
+function slideLeft(el){
+    console.log('sliding?')
+    if (elementInView(el)){
+        console.log('sliding!')
+        el.classList.add('slide-left');
+        el.classList.remove('pre-slide-left');
     }
 };
